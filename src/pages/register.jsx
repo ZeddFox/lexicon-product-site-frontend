cbf39ccadec1c585d95b48f8a2008e97724c2d64
Zedd Fox <25223537+ZeddFox@users.noreply.github.com>
2025-05-15 11:39:11 +0200

    Initial Commit

---
+++ src/pages/register.jsx
@@ -0,0 +1,5 @@
+import { useForm } from 'react-hook-form';
+import '../styles/style.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { API_BASE_URL } from "../config";
---
+++ src/pages/shopcart.jsx
@@ -0,0 +1,4 @@
+import '../styles/style.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { API_BASE_URL } from "../config";
